7d46fcf7b8f51d418482c4dd7c56fdcb055b4dc7
zcla <zeclaudioaguiar@gmail.com>
2022-05-18 23:24:54 -0300

    Eventos arquidiocesanos

--- js/mesce.js
+++ js/mesce.js
@@ -12,97 +12,6 @@ let data = null;
 // TODO https://www.jsviews.com/
 
 class Menu {
-	static async escala() {
-		GuiUtils.mensagensLimpa();
-		GuiUtils.conteudoLimpa();
-		
-		const table = $('<table class="table table-sm table-bordered table-striped escala">');
-		GuiUtils.conteudoAdiciona(table);
-		
-		const escala = await Escala.mostraMissas();
-
-		const thead = $('<thead>');
-		thead.append(
-				$('<tr>')
-						.append($('<th>').append('Missa (' + escala.escala.length + ')'))
-						.append($('<th>').append('Escalar (' + escala.escalar + ')'))
-						.append($('<th>').append('Ministros escalados (' + escala.escalados + ')'))
-		);
-		table.append(thead);
-
-		const tbody = $('<tbody>');
-		
-		for (const item of escala.escala) {
-			let escalados = "";
-			for (const ministro of item.escalados) {
-				escalados += ministro + ", ";
-			}
-			escalados = escalados.substring(0, escalados.length - 2);
-			
-			tbody
-					.append($('<tr>')
-							.append($('<td class="missa">')
-									.append(item.nome))
-							.append($('<td class="escalar">')
-									.append(item.escalar))
-							.append($('<td class="escalados">')
-									.append(escalados)
-									.append($('<span class="badge float-end ' + item.situacao() + '">')
-											.append(item.situacao()))));
-		}
-
-		table.append(tbody);
-	}
-
-	static async ministrosLista() {
-		GuiUtils.mensagensLimpa();
-		GuiUtils.conteudoLimpa();
-
-		const table = $('<table class="table table-sm table-bordered table-striped table-hover ministros">');
-		GuiUtils.conteudoAdiciona(table);
-
-		const lista = await Ministro.lista();
-		
-		const thead = $('<thead>');
-		thead
-				.append($('<tr>')
-						.append($('<th rowspan="2">').append('Nome (' + lista.total + ')'))
-						.append($('<th rowspan="2">').append('Aniversário'))
-						.append($('<th colspan="2">').append('Disponibilidade')))
-				.append($('<tr>')
-						.append($('<th>').append('Enfermos (' + lista.disponibilidade.enfermos + ')'))
-						.append($('<th>').append('Missas (' + lista.disponibilidade.missas + ')')));
-		table.append(thead);
-
-		const tbody = $('<tbody>');
-
-		for (const ministro of lista.ministros) {
-			let nomeFormatado = ministro.nomeFormatado();
-			const nome = $('<span>').append(nomeFormatado);
-			if (ministro.funcao) {
-				nome.append($('<span class="badge float-end funcao">')
-						.append(ministro.funcao));
-			}
-			const disponibilidadeMissas = $('<span>').append(GuiUtils.simboloCondicao(ministro.disponibilidade.includes('missas')));
-			if (ministro.escaladoMissas) {
-				disponibilidadeMissas.append(' (' + ministro.escaladoMissas + ')')
-			}
-
-			tbody
-					.append($('<tr>')
-							.append($('<td class="nome">')
-									.append(nome))
-							.append($('<td class="aniversario">')
-									.append(ministro.aniversario))
-							.append($('<td class="disponibilidade_enfermos">')
-									.append(GuiUtils.simboloCondicao(ministro.disponibilidade.includes('enfermos'))))
-							.append($('<td class="disponibilidade_missas">')
-									.append(disponibilidadeMissas)));
-		}
-
-		table.append(tbody);
-	}
-	
 	static async agenda(data)	{
 		if (GuiUtils.isMobile()) {
 			await Menu.agendaDia(data);
@@ -282,8 +191,220 @@ class Menu {
 		}
 		table.append(tbody);
 	}
+
+	static async escala() {
+		GuiUtils.mensagensLimpa();
+		GuiUtils.conteudoLimpa();
+		
+		const table = $('<table class="table table-sm table-bordered table-striped escala">');
+		GuiUtils.conteudoAdiciona(table);
+		
+		const escala = await Escala.mostraMissas();
+
+		const thead = $('<thead>');
+		thead.append(
+				$('<tr>')
+						.append($('<th>').append('Missa (' + escala.escala.length + ')'))
+						.append($('<th>').append('Escalar (' + escala.escalar + ')'))
+						.append($('<th>').append('Ministros escalados (' + escala.escalados + ')'))
+		);
+		table.append(thead);
+
+		const tbody = $('<tbody>');
+		
+		for (const item of escala.escala) {
+			let escalados = "";
+			for (const ministro of item.escalados) {
+				escalados += ministro + ", ";
+			}
+			escalados = escalados.substring(0, escalados.length - 2);
+			
+			tbody
+					.append($('<tr>')
+							.append($('<td class="missa">')
+									.append(item.nome))
+							.append($('<td class="escalar">')
+									.append(item.escalar))
+							.append($('<td class="escalados">')
+									.append(escalados)
+									.append($('<span class="badge float-end ' + item.situacao() + '">')
+											.append(item.situacao()))));
+		}
+
+		table.append(tbody);
+	}
+
+	static async ministrosLista() {
+		GuiUtils.mensagensLimpa();
+		GuiUtils.conteudoLimpa();
+
+		const table = $('<table class="table table-sm table-bordered table-striped table-hover ministros">');
+		GuiUtils.conteudoAdiciona(table);
+
+		const lista = await Ministro.lista();
+		
+		const thead = $('<thead>');
+		thead
+				.append($('<tr>')
+						.append($('<th rowspan="2">').append('Nome (' + lista.total + ')'))
+						.append($('<th rowspan="2">').append('Aniversário'))
+						.append($('<th colspan="3">').append('Disponibilidade')))
+				.append($('<tr>')
+						.append($('<th>').append('Enfermos (' + lista.disponibilidade.enfermos + ')'))
+						.append($('<th>').append('Missas (' + lista.disponibilidade.missas + ')'))
+						.append($('<th>').append('Eventos arquidiocesanos (' + lista.disponibilidade.arquidiocese + ')')));
+		table.append(thead);
+
+		const tbody = $('<tbody>');
+
+		for (const ministro of lista.ministros) {
+			let nomeFormatado = ministro.nomeFormatado();
+			const nome = $('<span>').append(nomeFormatado);
+			if (ministro.funcao) {
+				nome.append($('<span class="badge float-end funcao">')
+						.append(ministro.funcao));
+			}
+			const disponibilidadeMissas = $('<span>').append(GuiUtils.simboloCondicao(ministro.disponibilidade.includes('missas')));
+			if (ministro.escaladoMissas) {
+				disponibilidadeMissas.append(' (' + ministro.escaladoMissas + ')')
+			}
+			const disponibilidadeArquidiocese = $('<span>').append(GuiUtils.simboloCondicao(ministro.disponibilidade.includes('arquidiocese')));
+
+			tbody
+					.append($('<tr>')
+							.append($('<td class="nome">')
+									.append(nome))
+							.append($('<td class="aniversario">')
+									.append(ministro.aniversario))
+							.append($('<td class="disponibilidade_enfermos">')
+									.append(GuiUtils.simboloCondicao(ministro.disponibilidade.includes('enfermos'))))
+							.append($('<td class="disponibilidade_missas">')
+									.append(disponibilidadeMissas))
+							.append($('<td class="disponibilidade_arquidiocese">')
+									.append(disponibilidadeArquidiocese)));
+		}
+
+		table.append(tbody);
+	}
+
+	static async compromissos(dias) {
+		if ((!dias) || (dias < 1)) {
+			dias = 14;
+		}
+
+		GuiUtils.mensagensLimpa();
+		GuiUtils.conteudoLimpa();
+		
+		const table = $('<table class="table table-sm table-bordered table-striped servico">');
+		GuiUtils.conteudoAdiciona(table);
+
+		const thead = $('<thead>');
+		thead.append(
+				$('<tr>')
+						.append($('<th>').append('Data'))
+						.append($('<th>').append('Hora'))
+						.append($('<th>').append('Evento'))
+						.append($('<th>').append('Ministros'))
+		);
+		table.append(thead);
+
+		const tbody = $('<tbody>');
+		const dataAtual = new Date((new Date()).toDateString());
+		const dataFinal = new Date(dataAtual);
+		dataFinal.setDate(dataFinal.getDate() + dias - 1);
+/*
+		const eventos = Evento.ordenaCronologicamente(await Evento.instanciaEventosNaEscalaDoPeriodo(dataAtual, dataFinal));
+
+		for (let dia = 0; dia < dias; dia++) {
+			for (const evento of eventos) {
+				if (evento.data.toDateString() == dataAtual.toDateString()) {
+					const escalados = $('<ul class="list-group list-group-horizontal">');
+					const situacao = $('<span class="badge float-end">');
+					let numEscalar = 0;
+					let listaEscalados = [];
+					let numEscalados = 0;
+					let ministrosSubstitutos = [];
+					
+					if (evento.itemEscala) {
+						numEscalar = evento.itemEscala.escalar;
+						listaEscalados = evento.itemEscala.escalados;
+					} else {
+						numEscalar = evento.escalar;
+						for (const escalado of evento.escalados) {
+							listaEscalados.push(await Ministro.buscaPorId(escalado)); // TODO deveria ser aqui mesmo??? Talvez tenha que ir pra dentro da classe, como ocorre no if acima
+						}
+					}
+					if (!listaEscalados) {
+						listaEscalados = [];
+					}
+					for (const ministro of listaEscalados) {
+						let afastado = false;
+						if (ministro.afastamentos) {
+							for (const afastamento of ministro.afastamentos) {
+								if (DateUtils.noIntervalo(dataAtual, DateUtils.stringToDate(afastamento.inicio), DateUtils.stringToDate(afastamento.fim))) {
+									afastado = true;
+									if (afastamento.substituto) {
+										for (const substituto of afastamento.substituto) {
+											if (DateUtils.mesmaData(DateUtils.stringToDate(substituto.data), dataAtual) && (substituto.idEscala == evento.itemEscala.id)) {
+												ministrosSubstitutos.push(await Ministro.buscaPorId(substituto.idMinistro));
+											}
+										}
+									}
+								}
+							}
+						}
+						if (afastado) {
+							escalados
+									.append($('<li class="list-group-item afastado">')
+											.append(ministro.nomeGuerra));
+						} else {
+							escalados
+									.append($('<li class="list-group-item">')
+											.append(ministro.nomeGuerra));
+							numEscalados++;
+						}
+					}
+
+					for (const ministroSubstituto of ministrosSubstitutos) {
+						escalados
+								.append($('<li class="list-group-item substituto">')
+										.append(ministroSubstituto.nomeGuerra));
+						numEscalados++;
+					}
+
+					if (numEscalados >= numEscalar) {
+						situacao.addClass('bg-success');
+						situacao.append('ok');
+					} else {
+						situacao.addClass('bg-danger');
+						const faltam = numEscalar - numEscalados;
+						situacao.append('falta' + (faltam == 1 ? '' : 'm') + ' ' + faltam);
+					}
+
+					tbody
+							.append($('<tr>')
+									.append($('<td class="data">')
+											.append(DateUtils.formataDDMMYYYYWWW(dataAtual)))
+									.append($('<td class="hora">')
+											.append(evento.hora))
+									.append($('<td class="evento">')
+											.append(evento.nome))
+									.append($('<td class="escalar">')
+											.append(numEscalar))
+									.append($('<td class="escalados">')
+											.append(situacao)
+											.append(escalados)));
+				}
+			}
+
+			dataAtual.setDate(dataAtual.getDate() + 1);
+		}
+*/
+
+		table.append(tbody);
+	}
 }
 
 $(document).ready(async function() {
-	Menu.agenda();
+	Menu.ministrosLista();
 });
