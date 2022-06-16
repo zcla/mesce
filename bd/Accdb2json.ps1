879aaa29742ec76726e95c51ce99b44a12e28689
zcla <zeclaudioaguiar@gmail.com>
2022-06-16 00:34:05 -0300

    Caminho correto na geração dos json

--- bd/Accdb2json.ps1
+++ bd/Accdb2json.ps1
@@ -92,7 +92,7 @@ ForEach ($afastamento In $bdAfastamento) {
     $jsonMinistro."$($afastamento.id)".afastamento += $a
 }
 
-$jsonMinistro | ConvertTo-Json -Depth 3 | Out-File .\data\ministro.json
+$jsonMinistro | ConvertTo-Json -Depth 3 | Out-File ..\data\ministro.json
 
 # -------------------------------------------------- evento.json
 
@@ -122,6 +122,6 @@ ForEach ($evento In $bdEvento) {
     $jsonEvento."$($evento.id)" = $e
 }
 
-$jsonEvento | ConvertTo-Json -Depth 3 | Out-File .\data\evento.json
+$jsonEvento | ConvertTo-Json -Depth 3 | Out-File ..\data\evento.json
 
 # TODO fazer
